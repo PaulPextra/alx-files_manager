42eac820a5f3e3e2d838c5704bab5cea07aaa376
Paul Okoli <paulokoli7@gmail.com>
2023-06-22 05:45:26 -0700

    4. Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 
@@ -12,6 +13,9 @@ const router = (app) => {
   route.get('/status', (request, response) => AppController.getStatus(request, response));
   route.get('/stats', (request, response) => AppController.getStats(request, response));
   route.post('/users', (request, response) => UsersController.postNew(request, response));
+  route.get('/connect', (request, response) => AuthController.getConnect(request, response));
+  route.get('/disconnect', (request, response) => AuthController.getDisconnect(request, response));
+  route.get('/users/me', (request, response) => UsersController.getMe(request, response));
 }
 
 export default router;
